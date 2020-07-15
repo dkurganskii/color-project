a106517861b5bb8ab4eaaeba964748fb80af9539
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-07-15 08:53:20 -0500

    move out styles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,36 +2,7 @@ import React, { Component } from 'react';
 import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
-
-const styles = {
-	root: {
-		backgroundColor: 'blue',
-		height: '100vh',
-		display: 'flex',
-		alignItems: 'flex-start',
-		justifyContent: 'center'
-	},
-	container: {
-		width: '50%',
-		display: 'flex',
-		alignItems: 'flex-start',
-		flexDirection: 'column',
-		flexWrap: 'wrap'
-	},
-	nav: {
-		display: 'flex',
-		width: '100%',
-		justifyContent: 'space-between',
-		color: 'white'
-	},
-	palettes: {
-		boxSizing: 'border-box',
-		width: '100%',
-		display: 'grid',
-		gridTemplateColumns: 'repeat(3, 30%)',
-		gridGap: '5%'
-	}
-};
+import styles from './styles/PaletteListStyles';
 
 class PaletteList extends Component {
 	goToPalette(id) {
@@ -48,12 +19,11 @@ class PaletteList extends Component {
 					<div className={classes.palettes}>
 						{palettes.map((palette) => (
 							<MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)} />
-						))};
+						))}
 					</div>
 				</div>
 			</div>
 		);
 	}
 }
-
 export default withStyles(styles)(PaletteList);
