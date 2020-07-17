52d78817697937c37329b2e7729578fc75009be3
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-07-17 13:47:59 -0500

    start to style palette nav

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -1,22 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
-import PaletteFormNav from './PaletteFormNav';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Button from '@material-ui/core/Button';
-import DraggableColorList from './DraggableColorList';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { ChromePicker } from 'react-color';
-import { arrayMove } from 'react-sortable-hoc';
 
 class ColorPickerForm extends Component {
 	constructor(props) {
@@ -48,15 +33,18 @@ class ColorPickerForm extends Component {
 			name: this.state.newColorName
 		};
 		this.props.addNewColor(newColor);
+		this.setState({ newColorName: '' });
 	}
+
 	render() {
 		const { paletteIsFull } = this.props;
+		const { currentColor, newColorName } = this.state;
 		return (
 			<div>
-				<ChromePicker color={this.state.currentColor} onChangeComplete={this.updateCurrentColor} />
+				<ChromePicker color={currentColor} onChangeComplete={this.updateCurrentColor} />
 				<ValidatorForm onSubmit={this.handleSubmit} ref="form">
 					<TextValidator
-						value={this.state.newColorName}
+						value={newColorName}
 						name="newColorName"
 						onChange={this.handleChange}
 						validators={[ 'required', 'isColorNameUnique', 'isColorUnique' ]}
@@ -68,7 +56,7 @@ class ColorPickerForm extends Component {
 						color="primary"
 						disabled={paletteIsFull}
 						style={{
-							backgroundColor: paletteIsFull ? 'grey' : this.state.currentColor
+							backgroundColor: paletteIsFull ? 'grey' : currentColor
 						}}
 					>
 						{paletteIsFull ? 'Palette Full' : 'Add Color'}
@@ -78,5 +66,4 @@ class ColorPickerForm extends Component {
 		);
 	}
 }
-
 export default ColorPickerForm;
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -16,6 +16,38 @@ import DraggableColorList from './DraggableColorList';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { ChromePicker } from 'react-color';
 import { arrayMove } from 'react-sortable-hoc';
+
+const drawerWidth = 400;
+
+const styles = (theme) => ({
+	root: {
+		display: 'flex'
+	},
+
+	appBar: {
+		transition: theme.transitions.create([ 'margin', 'width' ], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		}),
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		height: '64px'
+	},
+	appBarShift: {
+		width: `calc(100% - ${drawerWidth}px)`,
+		marginLeft: drawerWidth,
+		transition: theme.transitions.create([ 'margin', 'width' ], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		})
+	},
+	menuButton: {
+		marginLeft: 12,
+		marginRight: 20
+	},
+	navBtns: {}
+});
+
 class PaletteFormNav extends Component {
 	constructor(props) {
 		super(props);
@@ -36,7 +68,7 @@ class PaletteFormNav extends Component {
 		const { classes, open } = this.props;
 		const { newPaletteName } = this.state;
 		return (
-			<div>
+			<div className={classes.root}>
 				<CssBaseline />
 				<AppBar
 					position="fixed"
@@ -55,8 +87,10 @@ class PaletteFormNav extends Component {
 							<MenuIcon />
 						</IconButton>
 						<Typography variant="h6" color="inherit" noWrap>
-							Persistent drawer
+							Create a Palette
 						</Typography>
+					</Toolbar>
+					<div className="navBtns">
 						<ValidatorForm onSubmit={() => this.props.handleSubmit(newPaletteName)}>
 							<TextValidator
 								label="Palette Name"
@@ -69,17 +103,17 @@ class PaletteFormNav extends Component {
 							<Button variant="contained" color="primary" type="submit">
 								Save Palette
 							</Button>
-							<Link to="/">
-								<Button variant="contained" color="secondary">
-									Go Back
-								</Button>
-							</Link>
 						</ValidatorForm>
-					</Toolbar>
+						<Link to="/">
+							<Button variant="contained" color="secondary">
+								Go Back
+							</Button>
+						</Link>
+					</div>
 				</AppBar>
 			</div>
 		);
 	}
 }
 
-export default PaletteFormNav;
+export default withStyles(styles, { withTheme: true })(PaletteFormNav);
